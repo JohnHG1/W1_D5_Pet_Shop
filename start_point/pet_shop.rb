eed29de71036a71b9b56e7a9b89aa56d9ba1b3df
JohnHG1 <honisz@yahoo.com>
2020-01-17 15:38:51 +0000

    Seconf commit

---
+++ start_point/pet_shop.rb
@@ -0,0 +1,9 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+    # test checks function returns correct name value
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+    # test checks functions returns correct total_cash by entering into two hashes.
