0cec21459a04d802baca0bcf808b264f1ff9d82a
JohnHG1 <honisz@yahoo.com>
2020-01-17 16:30:06 +0000

    Third

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -3,16 +3,22 @@ def pet_shop_name(pet_shop)
 end
     # test checks function returns correct name value
 
-
 def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
     # test checks functions returns correct total_cash by entering into two hashes.
-
 
 def add_or_remove_cash(pet_shop, money)
   new_total_cash = pet_shop[:admin][:total_cash] += money
 end
   # test checks if function returns new value which +/- new sum to original total.
 
-  
+def add_or_remove_cash(pet_shop, money)
+    new_total_cash = pet_shop[:admin][:total_cash] += money
+end
+# test checks if function returns new value which +/- new sum to original total.
+
+def pets_sold(pet_shop)
+  return sold = pet_shop[:admin][:pets_sold]
+end
+  # test checks if function returns new value which +/- of @et sale.
