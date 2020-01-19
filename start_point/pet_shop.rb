ae6e1cbf2f620ac825497144cef822d13f9fe800
JohnHG1 <honisz@yahoo.com>
2020-01-19 00:45:13 +0000

    thirteen

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,38 +1,38 @@
   def pet_shop_name(pet_shop)
     return pet_shop[:name]
   end
-#     # test checks function returns correct name value
-#
+    # test checks function returns correct name value
+
   def total_cash(pet_shop)
     return pet_shop[:admin][:total_cash]
   end
-#     # test checks functions returns correct total_cash by entering into two hashes.
+     # test checks functions returns correct total_cash by entering into two hashes.
 #
   def add_or_remove_cash(pet_shop, money)
     new_total_cash = pet_shop[:admin][:total_cash] += money
   end
-#     # test checks if function returns new value which +/- new sum to original total.
-#
-    def add_or_remove_cash(pet_shop, money)
-      new_total_cash = pet_shop[:admin][:total_cash] += money
-    end
-#     # As above so could be blanked out.
-#
-def pets_sold(pet_shop)
-  return sold = pet_shop[:admin][:pets_sold]
-end
+    # test checks if function returns new value which +/- new sum to original total.
+
+    # def add_or_remove_cash(pet_shop, money)
+    #   new_total_cash = pet_shop[:admin][:total_cash] += money
+    # end
+     # as above - function commented-out as the previous function covers both tests.
+
+  def pets_sold(pet_shop)
+    return sold = pet_shop[:admin][:pets_sold]
+  end
 #     # test checks if function returns new value of number of pets sold by entering into an array within an array.
 #
   def increase_pets_sold(pet_shop, num)
     return pet_shop[:admin][:pets_sold]+=(num)
   end
 #     # tests checks if function returns a new value that reflects an increase in pet sales.
-#
+
   def stock_count(stock_count)
     return stock_count[:pets].count
   end
-#     # tests returns value
-#
+#     # tests returns level of stock value
+
   def pets_by_breed(pet_shop, breed)
     breeds_array = []
     for pet in pet_shop[:pets]
@@ -54,8 +54,8 @@ end
     end
     return breeds_array
   end
-#   #   #As function above so blanked out.
-#
+   #   #As function above so blanked out.
+
   def find_pet_by_name(pet_shop, pet_name)
     breeds_array = []
     for pet in pet_shop[:pets]
@@ -67,16 +67,16 @@ end
   end
     # def pet_by_breed (pet_shop, name) using a for loop function goes through hashes checking for "string" and stores finding, then stops at end of array.
 
-    def find_pet_by_name(pet_shop, pet_name)
-      breeds_array = []
-      for pet in pet_shop[:pets]
-        if pet_name == pet[:name]
-          return pet
-        end
-      end
-      return breeds_array
-    end
-      # as above - function could be deleted as the above function covers both tests.
+    # def find_pet_by_name(pet_shop, pet_name)
+    #   breeds_array = []
+    #   for pet in pet_shop[:pets]
+    #     if pet_name == pet[:name]
+    #       return pet
+    #     end
+    #   end
+    #   return breeds_array
+    # end
+      # as above - function commented-out as the previous function covers both tests.
 
   def find_pet_by_name(pet_shop, name)
     for pet in pet_shop[:pets]
@@ -86,25 +86,34 @@ end
   end
   # def pet_by_breed (pet_shop, name) using a for loop function goes through hashes checking for "string" and stores finding, then stops at end of array. In this case if name is not found then retrun "nil"
 
-  # def add_pet_to_stock(pet_shop, stock)
-  #   for pet in pet_shop[:pets][:new_pet] +=(num)
-  #     return count
+
+  def remove_pet_by_name(pet_shop, name)
+    for pet in pet_shop[:pets]
+      return pet.delete(:name) if name == pet[:name]
+    end
+  end
+  # Loop function goes through pets array checking string???
+
+  # def test_add_pet_to_stock(pet_shop, new_pet)
+  #   return pet_shop[:pets].push(new_pet)
   # end
 
   def customer_cash(customer)
     return customer[:cash]
   end
+  # test checks function returns correct cash value
 
-
-
-
-
-
+  # def remove_customer_cash()
   #
+  #
+  # end
+
+
    def customer_pet_count(customer)
      return customer[:pets].count
    end
+   # test checks function returns correct pet count value
 
 
 
-# # end
+ # end
